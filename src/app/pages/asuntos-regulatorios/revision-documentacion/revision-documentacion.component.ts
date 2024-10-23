04caaa9320094c446f39004f7ba79328fe2fc75a
Camila Neiza <castronjulianac@javeriana.edu.co>
2024-10-23 00:03:39 -0500

    Componente para el paso 3

---
+++ src/app/pages/asuntos-regulatorios/revision-documentacion/revision-documentacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-revision-documentacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './revision-documentacion.component.html',
+  styleUrl: './revision-documentacion.component.css'
+})
+export class RevisionDocumentacionComponent {
+
+}
