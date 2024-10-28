4b2e7f57e641aa20ea108d12159cb74f3423198b
florezandres <andres.f1850@gmail.com>
2024-10-28 15:59:09 -0500

    Perfil Usuario v1.3
    Mejoras esteticas de perfil y arreglos routing/permisos

--- src/app/components/asuntos-regulatorios/ar-header/ar-header.component.ts
+++ src/app/components/asuntos-regulatorios/ar-header/ar-header.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from '@/app/servicios/auth.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ar-header',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './ar-header.component.css'
 })
 export class ArHeaderComponent {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  logout() {
+    console.log('Logout');
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
--- src/app/components/solicitante/solicitante-header/solicitante-header.component.ts
+++ src/app/components/solicitante/solicitante-header/solicitante-header.component.ts
@@ -1,8 +1,21 @@
+import { AuthService } from '@/app/servicios/auth.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-solicitante-header',
   templateUrl: './solicitante-header.component.html',
   styleUrl: './solicitante-header.component.css',
 })
-export class SolicitanteHeaderComponent {}
+export class SolicitanteHeaderComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  logout() {
+    console.log('Logout');
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
