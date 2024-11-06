9c4a9119be281795d5083f439dec99d0eae4b53f
Juando26030 <juando02603@gmail.com>
2024-11-06 00:34:40 -0500

    barra de busqueda en solicitante funcionando

--- src/app/pages/asuntos-regulatorios/solicitudes/solicitudes.component.ts
+++ src/app/pages/asuntos-regulatorios/solicitudes/solicitudes.component.ts
@@ -5,21 +5,21 @@ import { EstadoTramite, Tramite } from '@/app/modelos/tramite';
 import { TramiteDTO } from '@/app/modelos/tramite.dto';
 import { TramiteService } from '@/app/servicios/tramite-regulatorio.service';
 import { Component } from '@angular/core';
-import {SolicitudDTO} from "@/app/modelos/solicitud.dto";
-import {SolicitudDEIService} from "@/app/servicios/solicitud-dei.service";
-import {Router} from "@angular/router";
+import { SolicitudDTO } from "@/app/modelos/solicitud.dto";
+import { SolicitudDEIService } from "@/app/servicios/solicitud-dei.service";
+import { Router } from "@angular/router";
 import * as XLSX from 'xlsx';
-
 
 @Component({
   selector: 'app-solicitudes',
   templateUrl: './solicitudes.component.html',
-  styleUrl: './solicitudes.component.css'
+  styleUrls: ['./solicitudes.component.css']
 })
 export class SolicitudesComponent {
   solicitudes: SolicitudDTO[] = [];
   filteredSolicitudes: SolicitudDTO[] = [];
   nombreSolicitante!: string;
+  searchTerm: string = ''; // Término de búsqueda
 
   // Opciones y valores seleccionados para los filtros
   tipoProductoOptions: string[] = [];
@@ -29,9 +29,9 @@ export class SolicitudesComponent {
   selectedTipoTramite: string = '';
   selectedEstadoTramite: string = '';
 
-  mostrarTabla1: boolean = true; // Controla la visibilidad de la tabla
+  mostrarTabla1: boolean = true;
 
-  constructor(private solicitudService: SolicitudDEIService, private router: Router){}
+  constructor(private solicitudService: SolicitudDEIService, private router: Router) {}
 
   ngOnInit(): void {
     this.getTramites();
@@ -40,7 +40,7 @@ export class SolicitudesComponent {
 
   // Navegar a la página de detalles del trámite
   goToTramiteInfo(tramiteId: number): void {
-    this.router.navigate(['/info-solicitud', tramiteId]); // asegúrate de usar 'info-tramite' aquí
+    this.router.navigate(['/info-solicitud', tramiteId]);
   }
 
   // Obtener la lista de trámites
@@ -63,13 +63,25 @@ export class SolicitudesComponent {
     this.tipoTramiteOptions = [...new Set(this.solicitudes.map(s => s.tramite.tipoTramite))];
   }
 
-  // Método para filtrar la lista de trámites en función de los filtros seleccionados
+  // Filtrar la lista de trámites según filtros y término de búsqueda
   filterTramites(): void {
+    const term = this.searchTerm.toLowerCase(); // Convertir a minúsculas
+
     this.filteredSolicitudes = this.solicitudes.filter(solicitud => {
-      const matchTipoProducto = this.selectedTipoProducto ? solicitud.tramite.tipoProducto === this.selectedTipoProducto : true;
-      const matchTipoTramite = this.selectedTipoTramite ? solicitud.tramite.tipoTramite === this.selectedTipoTramite : true;
-      const matchEstadoTramite = this.selectedEstadoTramite ? solicitud.tramite.estado === this.selectedEstadoTramite : true;
-      return matchTipoProducto && matchTipoTramite && matchEstadoTramite;
+      const tramite = solicitud.tramite;
+
+      // Prioridad de coincidencias
+      const matchSearchTerm = tramite
+        ? tramite.nombreProducto.toLowerCase().includes(term) ||  // nombreProducto
+        tramite.tipoProducto.toLowerCase().includes(term) ||    // tipoProducto
+        tramite.numeroRadicado?.toLowerCase().includes(term)    // numeroRadicado si existe
+        : false;
+
+      const matchTipoProducto = this.selectedTipoProducto ? tramite?.tipoProducto === this.selectedTipoProducto : true;
+      const matchTipoTramite = this.selectedTipoTramite ? tramite?.tipoTramite === this.selectedTipoTramite : true;
+      const matchEstadoTramite = this.selectedEstadoTramite ? tramite?.estado === this.selectedEstadoTramite : true;
+
+      return matchSearchTerm && matchTipoProducto && matchTipoTramite && matchEstadoTramite;
     });
   }
 
@@ -103,67 +115,59 @@ export class SolicitudesComponent {
 
   isOnlyEstadoActive(): boolean {
     return (
-      this.selectedEstadoTramite && // El estado está seleccionado
-      !this.selectedTipoProducto && // No hay tipo de producto seleccionado
-      !this.selectedTipoTramite // No hay tipo de trámite seleccionado
+      this.selectedEstadoTramite &&
+      !this.selectedTipoProducto &&
+      !this.selectedTipoTramite
     );
   }
 
   exportarAExcel(): void {
-
     const datosExcel = this.filteredSolicitudes.map(s => ({
-      Fecha : s.fechaSolicitud,
+      Fecha: s.fechaSolicitud,
       Solicitante: s.solicitante.nombre,
-      'Tipo de Producto' : s.tramite.tipoProducto,
-      'Tipo de Trámite' : s.tramite.tipoTramite,
+      'Tipo de Producto': s.tramite.tipoProducto,
+      'Tipo de Trámite': s.tramite.tipoTramite,
       Estado: s.tramite.estado,
     }));
 
-
-    // Convertir la lista de drogas a una hoja de trabajo
     const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(datosExcel);
 
-// Estilo para el encabezado
-const headerStyle = {
-  font: {
-      bold: true,
-      color: { rgb: 'FFFFFF' }, // Texto blanco
-      sz: 14, // Tamaño de fuente
-  },
-  fill: {
-      fgColor: { rgb: '0070C0' }, // Color de fondo azul
-  },
-  alignment: {
-      horizontal: 'center',
-  }
-};
+    // Estilo para el encabezado
+    const headerStyle = {
+      font: {
+        bold: true,
+        color: { rgb: 'FFFFFF' },
+        sz: 14,
+      },
+      fill: {
+        fgColor: { rgb: '0070C0' },
+      },
+      alignment: {
+        horizontal: 'center',
+      }
+    };
 
-// Aplicar estilo al encabezado
-const headerCells = ['A1', 'B1', 'C1', 'D1', 'E1']; // Celdas del encabezado
-headerCells.forEach(cell => {
-  if (!ws[cell]) {
-      ws[cell] = {};
-  }
-  ws[cell].s = headerStyle; // Aplicar el estilo
-});
+    // Aplicar estilo al encabezado
+    const headerCells = ['A1', 'B1', 'C1', 'D1', 'E1'];
+    headerCells.forEach(cell => {
+      if (!ws[cell]) {
+        ws[cell] = {};
+      }
+      ws[cell].s = headerStyle;
+    });
 
-// Ajustar el ancho de las columnas
-ws['!cols'] = [
-  { wch: 20 }, // Ancho para 'Fecha'
-  { wch: 30 }, // Ancho para 'Solicitante'
-  { wch: 25 }, // Ancho para 'Tipo de Producto'
-  { wch: 25 }, // Ancho para 'Tipo de Trámite'
-  { wch: 20 }, // Ancho para 'Estado'
-];
-    
-    // Crear un libro de trabajo
+    // Ajustar el ancho de las columnas
+    ws['!cols'] = [
+      { wch: 20 },
+      { wch: 30 },
+      { wch: 25 },
+      { wch: 25 },
+      { wch: 20 },
+    ];
+
+    // Crear libro de trabajo y exportar
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    
-    // Añadir la hoja de trabajo al libro
     XLSX.utils.book_append_sheet(wb, ws, 'Tramites');
-    
-    // Generar el archivo Excel
-    const excelFileName = 'lista_de_solicitudes.xlsx';
-    XLSX.writeFile(wb, excelFileName);
+    XLSX.writeFile(wb, 'lista_de_solicitudes.xlsx');
   }
 }
